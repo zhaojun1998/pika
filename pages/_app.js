c996154ca56eaeda7349943861cab743c8d4fe26
zhaojun <873019219@qq.com>
2022-04-19 17:26:16 +0800

    修改字体库为国内

--- pages/_app.js
+++ pages/_app.js
@@ -45,14 +45,14 @@ function MyApp({ Component, pageProps }) {
           property="twitter:image"
           content="https://pika.rishimohan.me/meta-image.png"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.geekzu.org" />
         <link
           rel="preconnect"
-          href="https://fonts.gstatic.com"
+          href="https://gapis.geekzu.org/g-fonts/"
           crossOrigin={true}
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;600;700;900&display=swap"
+          href="https://fonts.geekzu.org/css2?family=Mulish:wght@400;600;700;900&display=swap"
           rel="stylesheet"
         />
         <link
